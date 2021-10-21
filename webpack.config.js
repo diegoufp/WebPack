045e3a7cc0ae090733b7819dc251d22ff690ea59
sann <diegofernandezp001@gmail.com>
2021-10-20 23:05:43 -0500

    Primer commit, se crea base de webpack

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MinicssExtractPlugin = require("mini-css-extract-plugin");
+
+
+module.exports ={
+    module: {
+        rules: [
+            {
+                test: /\.js$/i,
+                exclude: /node_modules/,
+                use: {
+                    loader:"babel-loader"
+                }
+            },
+            {
+                test: /\.html$/i,
+                use:[
+                    {
+                        loader:"html-loader",
+                        options: {
+                            minimize:true
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.css$/i,
+                use: [MinicssExtractPlugin.loader, "css-loader"]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        }),
+        new MinicssExtractPlugin()
+    ]
+}
